2ca15759ef94a5e545af546a97d407fbe75e026d
SANTOSH HUMNEKAR <santosh.humnekar@gmail.com>
2017-04-16 17:04:09 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,12 @@
+export function sum(x, y) {
+  return x + y;
+}
+export function Subtract(x, y) {
+  return x - y;
+}
+export function Multiply(x, y) {
+  return x + y;
+}
+export function Square(x) {
+  return x * x;
+}
---
+++ Main.js
@@ -0,0 +1,8 @@
+import {sum, Subtract,Multiply,Square} as lib from 'Calculator'
+
+
+console.log(lib.sum(1,2));
+console.log(lib.Subtract(1,2));
+console.log(lib.Multiply(1,2));
+console.log(lib.Square(1,2));
+
